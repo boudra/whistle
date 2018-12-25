1972ded37131d0b9d23c5037be1b9a7e77cd9974
Mohamed Boudra <boudra.moha@gmail.com>
2018-12-25 23:40:23 +0100

    add http server

--- lib/whistle/application.ex
+++ lib/whistle/application.ex
@@ -1,15 +1,52 @@
 defmodule Whistle.Application do
   use Application
 
+  @http_server Application.get_env(:whistle, :server)
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
       supervisor(Phoenix.PubSub.PG2, [Whistle.PubSub, []]),
       worker(Whistle.ProgramRegistry, [Whistle.ProgramRegistry])
-    ]
+    ] ++ http_server(@http_server)
 
     opts = [strategy: :one_for_one, name: MyApp.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp http_server(nil) do
+    []
+  end
+
+  defp http_server(opts) do
+    [
+      Plug.Cowboy.child_spec(
+        scheme: Keyword.get(opts, :scheme, :http),
+        plug: nil,
+        options: [
+          dispatch: dispatch(Keyword.get(opts, :socket_handlers, %{}), Keyword.get(opts, :plug, nil)),
+          port: Keyword.get(opts, :port, 4000)
+        ]
+      )
+    ]
+  end
+
+  defp dispatch(handlers, plug) do
+    socket_handlers =
+      Enum.map(handlers, fn {path, router} ->
+        {path, Whistle.SocketHandler, {router, []}}
+      end)
+
+    plug_handler =
+      if is_nil(plug) do
+        []
+      else
+        [{:_, Plug.Cowboy.Handler, {plug, []}}]
+      end
+
+    [
+      {:_, socket_handlers ++ plug_handler}
+    ]
   end
 end
--- lib/whistle/program.ex
+++ lib/whistle/program.ex
@@ -1,6 +1,15 @@
 defmodule Whistle.Program do
   alias Whistle.Socket
 
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Whistle.Program
+
+      alias Whistle.Html
+      import Whistle.Socket
+    end
+  end
+
   @callback init(map()) :: {:ok, Whistle.state()}
   @callback authorize(Whistle.state(), Socket.t(), map()) :: {:ok, Socket.t()} | {:error, any()}
   @callback update(Whistle.message(), Whistle.state(), Socket.t()) :: {:ok, Whistle.state(), Socket.t()}
