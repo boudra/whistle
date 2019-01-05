5641676aa445c52545a01cf3c769972af03957a3
Mohamed boudra <boudra.moha@gmail.com>
2019-01-05 14:35:38 +0100

    lower required elixir version to 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Whistle.MixProject do
     [
       app: :whistle,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
