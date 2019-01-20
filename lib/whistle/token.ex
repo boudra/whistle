862092ddce266fcea26c88e50b094a9cfd6d1c23
Mohamed boudra <boudra.moha@gmail.com>
2019-01-20 18:50:38 +0100

    simplify token

--- lib/whistle/token.ex
+++ lib/whistle/token.ex
@@ -2,30 +2,24 @@ defmodule Whistle.Token do
   alias Plug.Crypto.MessageEncryptor
   alias Plug.Crypto.KeyGenerator
 
-  def sign(secret_key_base, salt, data, opts \\ []) when is_binary(salt) do
-    {signed_at_seconds, key_opts} = Keyword.pop(opts, :signed_at)
+  def sign(secret_key_base, encrypt_salt, sign_salt, data) do
+    secret = KeyGenerator.generate(secret_key_base, encrypt_salt)
+    sign_secret = KeyGenerator.generate(secret_key_base, sign_salt)
 
-    secret = KeyGenerator.generate(secret_key_base, salt)
-    sign_secret = KeyGenerator.generate(secret_key_base, "test")
-
-    signed_at_ms = if signed_at_seconds, do: trunc(signed_at_seconds * 1000), else: now_ms()
-
-    %{data: data, signed: signed_at_ms}
+    %{data: data, signed: now_ms()}
     |> :erlang.term_to_binary()
     |> MessageEncryptor.encrypt(secret, sign_secret)
   end
 
-  def verify(secret_key_base, salt, token, opts \\ [])
-
-  def verify(secret_key_base, salt, token, opts) when is_binary(salt) and is_binary(token) do
-    secret = KeyGenerator.generate(secret_key_base, salt)
-    sign_secret = KeyGenerator.generate(secret_key_base, "test")
+  def verify(secret_key_base, encrypt_salt, sign_salt, token, max_age \\ :infinity) do
+    secret = KeyGenerator.generate(secret_key_base, encrypt_salt)
+    sign_secret = KeyGenerator.generate(secret_key_base, sign_salt)
 
     case MessageEncryptor.decrypt(token, secret, sign_secret) do
       {:ok, message} ->
         %{data: data, signed: signed} = Plug.Crypto.safe_binary_to_term(message)
 
-        if expired?(signed, Keyword.get(opts, :max_age, :infinity)) do
+        if expired?(signed, max_age) do
           {:error, :expired}
         else
           {:ok, data}
