ebe99e96a1ba98ff447b24741d8367cd49be4847
Mohamed boudra <boudra.moha@gmail.com>
2019-01-27 23:41:51 +0100

    bump to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Whistle.MixProject do
   def project do
     [
       app: :whistle,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
